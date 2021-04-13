a20c2c7b4747e8d80ee6f35bccbad4a45cc29ebc
shaalwan <ssalwan20@gmail.com>
2021-04-13 10:12:44 +0530

    13-04-2021

---
+++ 32.LinelandMail.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;cin>>n;
+    signed long long int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    
+    cout<<arr[1]-arr[0]<<" "<<arr[n-1]-arr[0]<<endl;
+    for(int i=1;i<n-1;i++)
+        cout<<min((arr[i]-arr[i-1]),(arr[i+1]-arr[i]))<<" "<<max((arr[i]-arr[0]),(arr[n-1]-arr[i]))<<endl;
+    cout<<arr[n-1]-arr[n-2]<<" "<<arr[n-1]-arr[0];
+    return 0;
+}
---
+++ 33.MahmoudandLongestUncommonSubsequence.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1,s2;cin>>s1>>s2;
+    if(s1==s2)
+        cout<<-1;
+    else
+        cout<<max(s1.length(),s2.length());
+    return 0;
+}
